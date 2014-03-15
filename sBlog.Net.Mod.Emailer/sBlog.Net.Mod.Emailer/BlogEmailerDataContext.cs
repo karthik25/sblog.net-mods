b951945231940ce26777257643df397e383ee24b
Karthik Ananthapadmanaban <karthika25@gmail.com>
2014-03-14 21:46:44 -0400

    added data context

---
+++ sBlog.Net.Mod.Emailer/sBlog.Net.Mod.Emailer/BlogEmailerDataContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace sBlog.Net.Mod.Emailer
+{
+    public class BlogEmailerDataContext : DbContext
+    {
+        private IDbSet<Post> _posts; 
+
+        public BlogEmailerDataContext()
+            : base("server=localhost;Database=sblog;user id=msuser1;password=msuser1;")
+        {
+            
+        }
+
+        public string GetUrlForPost()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetUrlForPage()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ sBlog.Net.Mod.Emailer/sBlog.Net.Mod.Emailer/Post.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace sBlog.Net.Mod.Emailer
+{
+    public class Post
+    {
+        [Key]
+        public int PotID { get; set; }
+        public string PostUrl { get; set; }
+        public short EntryType { get; set; }
+        public DateTime PostAddedDate { get; set; }
+    }
+}
